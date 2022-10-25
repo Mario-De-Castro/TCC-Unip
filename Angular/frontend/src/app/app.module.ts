3cc9969a20bf9b495a231b840d242440d06a6d5f
Paulo Gramacho <neto.paulo.po@gmail.com>
2022-10-25 01:37:11 -0300

    add tootlip

--- Angular/frontend/src/app/app.module.ts
+++ Angular/frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     FormsModule,
     GoogleMapsModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/frontend/src/app/shared/shared.module.ts
+++ Angular/frontend/src/app/shared/shared.module.ts
@@ -1,16 +1,24 @@
+import { MtxTooltipModule } from '@ng-matero/extensions/tooltip';
 import { AppComponent } from '../app.component';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { GoogleMapsModule } from '@angular/google-maps';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, GoogleMapsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    GoogleMapsModule,
+    MatTooltipModule,
+    MtxTooltipModule
+  ],
   exports: [HomeComponent],
   providers: [],
   bootstrap: [AppComponent],
